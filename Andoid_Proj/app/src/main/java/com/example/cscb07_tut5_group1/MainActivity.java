da0c1c645f43f0bd19c9ad06efeaa9623684c8f1
Chloepan33 <zypan03@gmail.com>
2022-11-20 14:28:58 -0500

    UI design for sign up page
    roughly finish UI design for sign up page. Issue with displaying role selection has not resolved

--- Andoid_Proj/app/src/main/java/com/example/cscb07_tut5_group1/MainActivity.java
+++ Andoid_Proj/app/src/main/java/com/example/cscb07_tut5_group1/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.cscb07_tut5_group1;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -12,9 +17,11 @@ import androidx.navigation.ui.NavigationUI;
 
 import com.example.cscb07_tut5_group1.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements
+        AdapterView.OnItemSelectedListener{
 
     private ActivityMainBinding binding;
+    private String[] roles = {"Student","Administrator"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +39,28 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+
+
+        //Getting the instance of Spinner and applying OnItemSelectedListener on it
+        Spinner spin = (Spinner) findViewById(R.id.role);
+        spin.setOnItemSelectedListener(this);
+
+        //Creating the ArrayAdapter instance having the roles list
+        ArrayAdapter aa = new ArrayAdapter(this,android.R.layout.simple_spinner_item,roles);
+        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //Setting the ArrayAdapter data on the Spinner
+        spin.setAdapter(aa);
+    }
+
+
+    //Performing action onItemSelected and onNothing selected
+    @Override
+    public void onItemSelected(AdapterView<?> arg0, View arg1, int position, long id) {
+        Toast.makeText(getApplicationContext(),roles[position] , Toast.LENGTH_LONG).show();
+    }
+    @Override
+    public void onNothingSelected(AdapterView<?> arg0) {
+        // TODO Auto-generated method stub
     }
 
 }
