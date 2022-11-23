ca779001662f51bbe1ddacdad54e1aa23f902239
Zheyuan Wei <zheyuan.wei2003@gmail.com>
2022-11-23 00:17:07 -0500

    From Qihui Li CoursesTaken added by Qihui Li; Test_Activity modified by Qihui Li.
    Debugged by Zheyuan Wei, now CoursesTaken works fine;
    Login_Activity remains unfinished.

--- TimeLiner/app/src/main/java/com/example/sign_up/Login_Activity.java
+++ TimeLiner/app/src/main/java/com/example/sign_up/Login_Activity.java
@@ -1,29 +1,145 @@
 package com.example.sign_up;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class Login_Activity extends AppCompatActivity{
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+
+
+// import MD5
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+// import R
+import com.example.sign_up.R;
+
+// import Log
+import android.util.Log;
+
+public class Login_Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_page);
 
-        Button btn = (Button)findViewById(R.id.signup);
-
-        btn.setOnClickListener(new View.OnClickListener() {
+        // Goto Sign Up Page
+        Button goto_signup_btn = (Button) findViewById(R.id.signup);
+        goto_signup_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(Login_Activity.this, Signup_Activity.class));
             }
         });
-      }
 
+        // Login Part
+        DatabaseReference database = FirebaseDatabase.getInstance().getReference();
+        Button login_btn = (Button) findViewById(R.id.Login_Btn);
+        EditText username = (EditText) findViewById(R.id.Username_Login);
+        EditText password = (EditText) findViewById(R.id.Password_Login);
 
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String txt_username = username.getText().toString();
+                String txt_password = password.getText().toString();
 
+                if (TextUtils.isEmpty(txt_username)) {
+                    Toast.makeText(Login_Activity.this, "Empty Username!",
+                            Toast.LENGTH_SHORT).show();
+                } else if (txt_password.length() < 8) {
+                    Toast.makeText(Login_Activity.this,
+                            "Password too short! Should be more than 8 characters.",
+                            Toast.LENGTH_SHORT).show();
+                } else {
+                    loginStudent(database, txt_username, txt_password);
+                }
+            }
+        });
+    }
+
+    private String md5(String input) {
+        try {
+            // Create MD5 Hash
+            MessageDigest digest = java.security.MessageDigest.getInstance("MD5");
+            digest.update(input.getBytes());
+            byte messageDigest[] = digest.digest();
+
+            // Create Hex String
+            StringBuilder hexString = new StringBuilder();
+            for (byte aMessageDigest : messageDigest) {
+                String h = Integer.toHexString(0xFF & aMessageDigest);
+                while (h.length() < 2)
+                    h = "0" + h;
+                hexString.append(h);
+            }
+            return hexString.toString();
+
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    private void loginStudent(DatabaseReference database, String txt_username, String txt_password) {
+
+        database.get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+
+                    @Override
+                    public void onComplete(@NonNull Task<DataSnapshot> snapshot) {
+
+                        // database.child("DATABASE").child("STUDENTS").child(txt_username)
+                        // .addListenerForSingleValueEvent(new ValueEventListener() {
+                        // @Override
+                        // public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                        DataSnapshot data_snap = snapshot.getResult().child("DATABASE").child("STUDENTS");
+
+                        if (!snapshot.isSuccessful()) {
+                            Log.e("firebase", "Error getting data", snapshot.getException());
+                            Toast.makeText(Login_Activity.this, "Error getting data",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                        else if (!data_snap.hasChild(txt_username)) {
+                            Log.e("firebase", "User not found");
+                            Toast.makeText(Login_Activity.this, "User not found",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                        else {
+                            // HashMap<String, Object> student = new HashMap<>();
+                            String pass_md5 = md5(txt_password);
+                            String pass_md5_salt = md5(txt_username + pass_md5);
+                            if (data_snap.child("password").getValue().toString().equals(pass_md5) &&
+                                    snapshot.getResult().child("password_salt").getValue().toString()
+                                            .equals(pass_md5_salt)) {
+                                Log.i("Login", "Login Successful");
+                                Toast.makeText(Login_Activity.this, "Login Successful!",
+                                        Toast.LENGTH_SHORT).show();
+                                // Waiting for the Student Page
+                                // startActivity(new Intent(Login_Activity.this, StudentActivity.class));
+                            } else {
+                                Log.e("Login", "Password Wrong!");
+                                Toast.makeText(Login_Activity.this, "Wrong Password!",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    }
+                });
+    }
 }
