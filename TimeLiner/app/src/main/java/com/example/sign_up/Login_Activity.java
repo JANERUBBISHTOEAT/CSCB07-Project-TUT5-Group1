0643ab1d1248c2dd7ce73358cf89bd2654c9b5f6
Chloekyuu <xinzi.huang@mail.utoronto.ca>
2022-11-29 14:10:14 -0500

    Update Login_Activity.java

--- TimeLiner/app/src/main/java/com/example/sign_up/Login_Activity.java
+++ TimeLiner/app/src/main/java/com/example/sign_up/Login_Activity.java
@@ -39,7 +39,7 @@ public class Login_Activity extends AppCompatActivity {
         setContentView(R.layout.login_page);
 
         // Goto Sign Up Page
-        Button goto_signup_btn = (Button) findViewById(R.id.signup);
+        Button goto_signup_btn = findViewById(R.id.signup);
         goto_signup_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -49,16 +49,18 @@ public class Login_Activity extends AppCompatActivity {
 
         // Login Part
         DatabaseReference database = FirebaseDatabase.getInstance().getReference();
-        Button login_btn = (Button) findViewById(R.id.Login_Btn);
-        EditText username = (EditText) findViewById(R.id.Username_Login);
-        EditText password = (EditText) findViewById(R.id.Password_Login);
+        Button login_btn = findViewById(R.id.Login_Btn);
+        EditText username = findViewById(R.id.Username_Login);
+        EditText password = findViewById(R.id.Password_Login);
 
+        // If the user click the login_btn
         login_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String txt_username = username.getText().toString();
                 String txt_password = password.getText().toString();
 
+                // Validate the input
                 if (TextUtils.isEmpty(txt_username)) {
                     Toast.makeText(Login_Activity.this, "Empty Username!",
                             Toast.LENGTH_SHORT).show();
@@ -73,6 +75,7 @@ public class Login_Activity extends AppCompatActivity {
         });
     }
 
+    // encrypting the password
     private String md5(String input) {
         try {
             // Create MD5 Hash
@@ -102,27 +105,27 @@ public class Login_Activity extends AppCompatActivity {
 
             @Override
             public void onComplete(@NonNull Task<DataSnapshot> snapshot) {
-                // database.child("DATABASE").child("STUDENTS").child(txt_username)
-                // .addListenerForSingleValueEvent(new ValueEventListener() {
-                // @Override
-                // public void onDataChange(@NonNull DataSnapshot snapshot) {
 
-                DataSnapshot data_snap = snapshot.getResult().child("DATABASE").child("STUDENTS");
+                // md5 the password based on user's input
+                String pass_md5 = md5(txt_password);
+                String pass_md5_salt = md5(txt_username + pass_md5);
+
+                // Get the data snapshot
+                DataSnapshot students = snapshot.getResult().child("DATABASE").child("STUDENTS");
+                DataSnapshot admins = snapshot.getResult().child("DATABASE").child("ADMINS");
 
                 if (!snapshot.isSuccessful()) {
                     Log.e("firebase", "Error getting data", snapshot.getException());
                     Toast.makeText(Login_Activity.this, "Error getting data",
                             Toast.LENGTH_SHORT).show();
-                } else if (!data_snap.hasChild(txt_username)) {
-                    Log.e("firebase", "User not found");
-                    Toast.makeText(Login_Activity.this, "User not found!",
-                            Toast.LENGTH_SHORT).show();
-                } else {
-                    // HashMap<String, Object> student = new HashMap<>();
-                    String pass_md5 = md5(txt_password);
-                    String pass_md5_salt = md5(txt_username + pass_md5);
-                    String pass_web = data_snap.child(txt_username).child("pass_hash").getValue().toString();
-                    String pass_salt_web = data_snap.child(txt_username).child("salt_hash").getValue().toString();
+                } else if (admins.hasChild(txt_username)) { // if the user is an administrator
+
+                    // Get the correct password from the database
+                    String pass_web = admins.child(txt_username).child("pass_hash").getValue().toString();
+                    String pass_salt_web = admins.child(txt_username).child("salt_hash").getValue().toString();
+
+                    // check if the password is correct.
+                    // If it is, jump to the admin_page. If not, toast a message.
                     if (pass_web.equals(pass_md5) && pass_salt_web.equals(pass_md5_salt)) {
                         Log.i("Login", "Login Successful");
                         Toast.makeText(Login_Activity.this, "Login Successful!",
@@ -134,6 +137,29 @@ public class Login_Activity extends AppCompatActivity {
                         Toast.makeText(Login_Activity.this, "Wrong Password!",
                                 Toast.LENGTH_SHORT).show();
                     }
+                } else if (students.hasChild(txt_username)) {   // if the user is a student
+
+                    // Get the correct password from the database
+                    String pass_web = students.child(txt_username).child("pass_hash").getValue().toString();
+                    String pass_salt_web = students.child(txt_username).child("salt_hash").getValue().toString();
+
+                    // check if the password is correct.
+                    // If it is, jump to the admin_page. If not, toast a message.
+                    if (pass_web.equals(pass_md5) && pass_salt_web.equals(pass_md5_salt)) {
+                        Log.i("Login", "Login Successful");
+                        Toast.makeText(Login_Activity.this, "Login Successful!",
+                                Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(Login_Activity.this, CoursesTaken_Activity.class));
+                        finish();
+                    } else {
+                        Log.e("Login", "Password Wrong!");
+                        Toast.makeText(Login_Activity.this, "Wrong Password!",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                } else { // If the user's name is not found in the database, toast a message.
+                    Log.e("firebase", "User not found");
+                    Toast.makeText(Login_Activity.this, "User not found!",
+                            Toast.LENGTH_SHORT).show();
                 }
             }
         });
