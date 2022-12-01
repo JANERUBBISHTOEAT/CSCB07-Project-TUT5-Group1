75002f89facb7ef0da63c2418913838eb357b815
Zheyuan Wei <zheyuan.wei2003@gmail.com>
2022-12-01 16:23:35 -0500

    Need implementation of the Planner function

--- TimeLiner/app/src/main/java/com/example/sign_up/AcademicYear.java
+++ TimeLiner/app/src/main/java/com/example/sign_up/AcademicYear.java
@@ -8,15 +8,72 @@ import java.util.ArrayList;
 
 
 public class AcademicYear {
+    
+    
+    public class Year{
+        ArrayList<String> winter;
+        ArrayList<String> summer;
+        ArrayList<String> fall;
+        
+        public Year(){
+            winter = new ArrayList<String>();
+            summer = new ArrayList<String>();
+            fall = new ArrayList<String>();
+        }
+        
+        public void addCourse(String courseCode, String semester){
+            if(semester.equals("Winter")){
+                winter.add(courseCode);
+            }
+            else if(semester.equals("Summer")){
+                summer.add(courseCode);
+            }
+            else if(semester.equals("Fall")){
+                fall.add(courseCode);
+            }
+        }
 
-    ArrayList<String> winter;
-    ArrayList<String> summer;
-    ArrayList<String> fall;
+        public ArrayList<String> getWinter() {
+            return winter;
+        }
 
-    public AcademicYear(){
-        this.winter = new ArrayList<String>();
-        this.summer = new ArrayList<String>();
-        this.fall = new ArrayList<String>();
+        public ArrayList<String> getSummer() {
+            return summer;
+        }
+
+        public ArrayList<String> getFall() {
+            return fall;
+        }
+
+        public void setWinter(ArrayList<String> winter) {
+            this.winter = winter;
+        }
+
+        public void setSummer(ArrayList<String> summer) {
+            this.summer = summer;
+        }
+
+        public void setFall(ArrayList<String> fall) {
+            this.fall = fall;
+        }
     }
 
+    ArrayList<Year> timeline;
+
+    public AcademicYear(){
+        timeline = new ArrayList<Year>();
+        timeline.add(new Year());
+    }
+
+    public void addCourse(String courseCode, int year, String semester){
+        timeline.get(year).addCourse(courseCode, semester);
+    }
+
+    public ArrayList<String> getYear(int year){
+        return timeline.get(year);
+    }
+
+    public ArrayList<Year> getTimeline() {
+        return timeline;
+    }
 }
--- TimeLiner/app/src/main/java/com/example/sign_up/Planner.java
+++ TimeLiner/app/src/main/java/com/example/sign_up/Planner.java
@@ -1,6 +1,7 @@
 package com.example.sign_up;
 
 import androidx.annotation.NonNull;
+import android.util.Log;
 
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.database.DataSnapshot;
@@ -11,210 +12,143 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
+// import Course.java
+import com.example.sign_up.Course;
+
+// import toast, makeText, and LENGTH_SHORT
+import android.widget.Toast;
+import android.widget.Toast.makeText;
+
 public class Planner {
-    ArrayList<AcademicYear> timeline;
     ArrayList<String> courseTaken;
     ArrayList<String> courseWanted;
 
-    // getTimetableList() returns a list of all the courses in the timetable
-    public ArrayList<String> getTimetableList() {
-        ArrayList<String> timetableList = new ArrayList<String>();
-        for (int i = 0; i < timeline.size(); i++) {
-            AcademicYear year = timeline.get(i);
-            timetableList.addAll(year.winter);
-            timetableList.addAll(year.summer);
-            timetableList.addAll(year.fall);
-        }
-        return timetableList;
-    }
-
-    //create an empty planner.
-    public Planner(){
-        timeline = new ArrayList<AcademicYear>();
-        timeline.add(new AcademicYear());
+    public Planner() {
         courseTaken = new ArrayList<String>();
         courseWanted = new ArrayList<String>();
     }
 
-    //how many years estimate to be taken.
-    public void addAcademicYear(int i){
-        for(int j = 0; j<i; j++) {
-            timeline.add(new AcademicYear());
+    public void addCourseTaken(String courseCode) {
+        courseTaken.add(courseCode);
+    }
+
+    public void addCourseWanted(String courseCode) {
+        courseWanted.add(courseCode);
+    }
+
+    public ArrayList<String> getCourseTaken() {
+        return courseTaken;
+    }
+
+    public ArrayList<String> getCourseWanted() {
+        return courseWanted;
+    }
+
+    public void setCourseTaken(ArrayList<String> courseTaken) {
+        this.courseTaken = courseTaken;
+    }
+
+    public void setCourseWanted(ArrayList<String> courseWanted) {
+        this.courseWanted = courseWanted;
+    }
+
+    // Main function to generate a planner
+    public ArrayList<AcademicYear> generatePlanner(ArrayList<Course> courseTaken,
+            ArrayList<Course> courseWanted, ArrayList<Course> courseList) {
+        // Create a planner
+        ArrayList<AcademicYear> planner = new ArrayList<AcademicYear>();
+
+        // Generate a planner
+        while (courseWanted.size() > 0) {
+            for (int i = 0; i < courseWanted.size(); i++) {
+                // Check if the course is taken
+                if (courseTaken.contains(courseWanted.get(i))) {
+                    // Add the course to the planner
+                    // TODO: Add the course to the planner
+
+                    // Remove the course from the wanted list
+                    courseWanted.remove(i);
+                }
+
+                // Check if the course has a prerequisite
+                else if (courseList.get(i).getPrerequisite().size() > 0) {
+                    for (int j = 0; j < courseList.get(i).getPrerequisite().size(); j++) {
+                        // Check if the prerequisite is taken
+                        if (courseTaken.contains(courseList.get(i).getPrerequisite().get(j))) {
+                            // Add the course to the planner
+                            // TODO: Add the course to the planner
+
+                            // Remove the course from the wanted list
+                            courseWanted.remove(i);
+                        } else {
+                            // Add the prerequisite to the wanted list
+                            courseWanted.add(courseList.get(i).getPrerequisite().get(j));
+
+                            // Remove the course from the wanted list
+                            courseWanted.remove(i);
+
+                            // Add the course to the taken list
+                            courseTaken.add(courseList.get(i).getCourseCode());
+
+                            // Add the course to the planner
+                            // TODO: Add the course to the planner
+                        }
+                    }
+                }
+            }
         }
     }
 
-    //given an arraylist of pairs of year and semesters; return the largest pair
-    @NonNull
-    public static ArrayList<Integer> findFurthestSession(@NonNull ArrayList<Integer> lis){
-        int year = 0;
-        int sem = 0;
-        for(int i = 0; i<lis.size()-1; i=i+2){
-            if(year == lis.get(i) && sem < lis.get(i+1)){
-                sem = lis.get(i+1);
-            }
-            if(year < lis.get(i)) {
-                year = lis.get(i);
-                sem = lis.get(i + 1);
-            }
-        }
-        ArrayList<Integer> j = new ArrayList<Integer>();
-        j.add(year);
-        j.add(sem);
-        return j;
-    }
+    public Planner(String username, int sem) {
+        courseTaken = new ArrayList<String>();
+        courseWanted = new ArrayList<String>();
 
-    //add all taken courses to the courseTaken arraylist of the planner.
-    public void addCourseTaken(String user){
-        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference()
-                .child("DATABASE").child("STUDENTS").child(user);
+        // Timeline is a AcademicYear object
+
+        // Get all courses from database
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
         databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                if(snapshot.exists() && snapshot.hasChild("course_taken")){
-                    courseTaken.clear();
-                    for(DataSnapshot dss: snapshot.child("course_taken").getChildren()){
-                        String t = dss.getValue(String.class);
-                        courseTaken.add(t);
+                if (snapshot.exists()) {
+                    // Get all courses from database
+                    ArrayList<Course> course_taken = new ArrayList<Course>();
+                    ArrayList<Course> course_wanted = new ArrayList<Course>();
+                    ArrayList<Course> courses = new ArrayList<Course>();
+
+                    // Get all courses from database
+                    for (DataSnapshot ds : snapshot.child("DATABASE").child("COURSE").getChildren()) {
+                        courses.add(ds.getValue(Course.class));
                     }
+
+                    // Get all courses taken by user
+                    if (snapshot.child("STUDENTS").child(username).child("course_taken").exists()) {
+                        for (DataSnapshot ds : snapshot.child("STUDENTS").child(username).child("course_taken")
+                                .getChildren()) {
+                            course_taken.add(ds.getValue(Course.class));
+                        }
+                    }
+
+                    // Get all courses wanted by user
+                    if (snapshot.child("STUDENTS").child(username).child("course_wanted").exists()) {
+                        for (DataSnapshot ds : snapshot.child("STUDENTS").child(username).child("course_wanted")
+                                .getChildren()) {
+                            course_wanted.add(ds.getValue(Course.class));
+                        }
+                    }
+
+                    // Plan courses
+                    ArrayList<AcademicYear> planner = new ArrayList<AcademicYear>();
+                    planner = generatePlanner(course_taken, course_wanted, courses);
                 }
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
-
+                Log.d("Database Error", error.getMessage());
+                // Toast.makeText(Planner_Activity.this, "Database Error",
+                // Toast.LENGTH_SHORT).show();
             }
         });
-    }
-
-    //add all wanted courses to the courseWanted arraylist of the planner.
-    public void addCourseWanted(String user){
-        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference()
-                .child("DATABASE").child("STUDENTS").child(user);
-        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                if(snapshot.exists() && snapshot.hasChild("course_want")){
-                    courseWanted.clear();
-                    for(DataSnapshot dss: snapshot.child("course_want").getChildren()){
-                        String t = dss.getValue(String.class);
-                        courseWanted.add(t);
-                    }
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });
-    }
-
-
-    //given a Course, a given year i, and a semester sem, find the next session available for
-    // the course, and add the course to the planner.
-    public ArrayList<Integer> addNextSession(Course course, int i, int sem){
-        //initialize arraylist j
-        ArrayList<Integer> j = new ArrayList<Integer>();
-
-        //check if course is taken, or course is invisible. Both will return first year, sem.
-        if(this.courseTaken.contains(course.courseCode) || !course.visible){
-            j.add(1);
-            j.add(sem);
-        }
-        //i = 0 if it is initial input. Return initial input.
-        if(i == 0){
-            j.add(i);
-            j.add(sem);
-            return j;
-        }
-        //if planner year not sufficient, add more years
-        if(i > this.timeline.size()){
-            this.addAcademicYear(i-this.timeline.size());
-        }
-        //add to semester
-        if(course.sessionOffered.winter && sem == 0){
-            //check if courseCode already in planner
-            if(!this.timeline.get(i).winter.contains(course.courseCode)) {
-                this.timeline.get(i).winter.add(course.courseCode);
-            }
-            j.add(i);
-            j.add(1);
-        } else if(course.sessionOffered.summer && sem <= 1){
-            if(!this.timeline.get(i).summer.contains(course.courseCode)) {
-                this.timeline.get(i).summer.add(course.courseCode);
-            }
-            j.add(i);
-            j.add(2);
-        } else if(course.sessionOffered.fall && sem <= 2) {
-            if(!this.timeline.get(i).fall.contains(course.courseCode)) {
-                this.timeline.get(i).fall.add(course.courseCode);
-            }
-            j.add(i+1);
-            j.add(0);
-        }
-        return j;
-    }
-
-
-    //add all courses to the Planner. For the initial input, int i = 0. int sem = 0 if next
-    // semester is winter; sem = 1 if next semester is summer; and sem = 2 if next semester is
-    // fall.
-    public ArrayList<Integer> addAllCoursePlanner(Course course, int i, int sem){
-        //induction: course has prerequisite course(s).
-        if(course.preRequisiteCourses.size() > 0){
-            ArrayList<Integer> lis = new ArrayList<Integer>();
-            for(int j = 0; j<course.preRequisiteCourses.size(); j++){
-                //for each prerequisite course c, check what time is available
-                Course c = new Course();
-                c.getFromDatabase(course.preRequisiteCourses.get(i), new Course.MyCallback() {
-                    @Override
-                    public void onCallback(Course course_callback) {
-                        c.setCourseName(course_callback.getCourseName());
-                        c.setCourseCode(course_callback.getCourseCode());
-                        c.setCourseDescription(course_callback.getCourseDescription());
-                        c.setSessionOffered(course_callback.getSessionOffered());
-                        c.setPreRequisiteCourses(course_callback.getPreRequisiteCourses());
-                        c.setVisible(course_callback.isVisible());
-                    }
-                });
-                lis.addAll(this.addAllCoursePlanner(c, i, sem));
-            }
-            int year = findFurthestSession(lis).get(0);
-            int semester = findFurthestSession(lis).get(1);
-            //return the available time for course.
-            return this.addNextSession(course, year, semester);
-        }
-        else{
-            //base case: course has no preRequisiteCourses. year i = 1, semester = sem
-            return this.addNextSession(course, 1, sem);
-        }
-
-    }
-
-    //give username of a student, the next semester sem which 0,1,2 represent winter, summer,fall
-    // respectively.
-    public Planner(String username, int sem){
-        timeline = new ArrayList<AcademicYear>();
-        timeline.add(new AcademicYear());
-        courseTaken = new ArrayList<String>();
-        courseWanted = new ArrayList<String>();
-        this.addCourseTaken(username);
-        this.addCourseWanted(username);
-        for(String s: this.courseWanted){
-            Course c = new Course();
-            c.getFromDatabase(s, new Course.MyCallback() {
-                @Override
-                public void onCallback(Course course_callback) {
-                    c.setCourseName(course_callback.getCourseName());
-                    c.setCourseCode(course_callback.getCourseCode());
-                    c.setCourseDescription(course_callback.getCourseDescription());
-                    c.setSessionOffered(course_callback.getSessionOffered());
-                    c.setPreRequisiteCourses(course_callback.getPreRequisiteCourses());
-                    c.setVisible(course_callback.isVisible());
-                }
-            });
-
-            this.addAllCoursePlanner(c, 1, sem);
-        }
     }
 }
