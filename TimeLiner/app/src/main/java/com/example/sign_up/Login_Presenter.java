a9eab6bf140ab638b6e3e9a88280963f8e0a2e80
Chloekyuu <xinzi.huang@mail.utoronto.ca>
2022-12-04 18:47:44 -0500

    Update refactored login activity

--- TimeLiner/app/src/main/java/com/example/sign_up/Login_Presenter.java
+++ TimeLiner/app/src/main/java/com/example/sign_up/Login_Presenter.java
@@ -1,28 +1,58 @@
 package com.example.sign_up;
 
+import android.content.Intent;
 import android.text.TextUtils;
+import android.util.Log;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 
 public class Login_Presenter {
     private Login_View view;
+    private DatabaseReference database;
 
-    public Login_Presenter(Login_View view) {
+    public Login_Presenter(Login_View view, DatabaseReference database) {
         this.view = view;
+        this.database = database;
     }
 
-    public void loginUser(DatabaseReference database, String txt_username, String txt_password) {
+    public void loginUser() {
+        String txt_username = view.getUserName();
+        String txt_password = view.getUserPassword();
+
         if (TextUtils.isEmpty(txt_username)) {
-            view.displayValidName();
+            view.displayMessage("Empty Username!");
         } else if (txt_password.length() < 8) {
-            view.displayValidPassword();
+            view.displayMessage("Password too short! Should be more than 8 characters.");
         } else {
             // md5 the password based on user's input
             String pass_md5 = view.displayMD5(txt_password);
             String pass_md5_salt = view.displayMD5(txt_username + pass_md5);
 
-            view.displayLogin(database, txt_username, pass_md5, pass_md5_salt);
+            database.get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<DataSnapshot> snapshot) {
+                    if (!snapshot.isSuccessful()) {
+                        view.displayMessage("Error getting data");
+                    } else {
+                        Intent intent = view.displayLogin(snapshot, txt_username, pass_md5, pass_md5_salt);
+                        if (intent == null) {
+                            view.displayMessage("User not found!");
+                        } else if (intent.hasExtra("wrongPassword")) {
+                            view.displayMessage("Wrong Password!");
+                        } else {
+                            view.displayMessage("Login Successful!");
+                            view.goToPage(intent);
+                        }
+                    }
+                }
+            });
         }
     }
 }
